1c367904eb3ae33ec92a634056524d4bea623e6a
Fredika Putra <fredikaputra6@gmail.com>
2019-11-23 09:34:26 +0800

    Change name

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-// define('BASEURL', 'http://localhost/paytrik');
-define('BASEURL', 'http://localhost/sattar');
+define('BASEURL', 'http://localhost/paytrik');
+// define('BASEURL', 'http://localhost/sattar');
 
 // DB
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-// define('DB_NAME', 'paytrik');
-define('DB_NAME', 'sattar');
+define('DB_NAME', 'paytrik');
+// define('DB_NAME', 'sattar');
