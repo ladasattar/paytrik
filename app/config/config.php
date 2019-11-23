5b93b91bf4b745f69f935f87b748d2f547672e11
Fredika Putra <fredikaputra6@gmail.com>
2019-11-23 09:27:59 +0800

    Adding automacally year

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,12 @@
 <?php
 
-define('BASEURL', 'http://localhost/paytrik');
+// define('BASEURL', 'http://localhost/paytrik');
+define('BASEURL', 'http://localhost/sattar');
 
 // DB
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'paytrik');
+// define('DB_NAME', 'paytrik');
+define('DB_NAME', 'sattar');
